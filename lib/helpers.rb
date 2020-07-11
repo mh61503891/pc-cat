2861af03068368bb7c3f474bedf6eb1de4480432
Masayuki Higashino <msyk@hgsn.info>
2020-07-12 04:17:14 +0900

    Fix warning: "already initialized constant" and "previous definition constant of  was here done"

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,23 +1,27 @@
 module VideoTag
 
-  BASE_URL = 'https://object-storage.tyo1.conoha.io/v1/nc_9dbd179936404f7482bfccf5368fddb7/pc-cat/'
+  def base_url
+    'https://object-storage.tyo1.conoha.io/v1/nc_9dbd179936404f7482bfccf5368fddb7/pc-cat/'
+  end
 
   def video_poster_url_for(content)
-    URI.join(BASE_URL, "#{content[:category_key]}/#{content[:key]}.png").to_s
+    URI.join(base_url, "#{content[:category_key]}/#{content[:key]}.png").to_s
   end
 
   def video_src_url_for(content)
-    URI.join(BASE_URL, "#{content[:category_key]}/#{content[:key]}.mp4").to_s
+    URI.join(base_url, "#{content[:category_key]}/#{content[:key]}.mp4").to_s
   end
 
 end
 
 module GitHub
 
-  BASE_URL = 'https://github.com/mh61503891/pc-cat/blob/development/content/tracks/'
+  def base_url
+    'https://github.com/mh61503891/pc-cat/blob/development/content/tracks/'
+  end
 
   def link_to_edit_on_github(content)
-    URI.join(BASE_URL, "#{content[:category_key]}/#{content[:key]}.srt").to_s
+    URI.join(base_url, "#{content[:category_key]}/#{content[:key]}.srt").to_s
   end
 
 end
